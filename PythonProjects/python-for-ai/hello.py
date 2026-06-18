bd1b0b410af5d17ea2892c3b85045fb09fb146ee
Mehedi Hasan <mehedihasandb0@gmail.com>
2026-06-18 19:32:24 +0600

    logical operator add

--- PythonProjects/python-for-ai/hello.py
+++ PythonProjects/python-for-ai/hello.py
@@ -16,9 +16,62 @@ can_vote = age >= 18
 
 age = 25
 job =  True
+adult = True
+child = age <= 20
+is_adult = not child
 
-can_marry = age >= 20 and job 
+can_marry = age >= 20 and job or adult and not is_adult
 print(can_marry)  # True
+
+
+get_bike = age >= 25 and job
+print(get_bike)  # False
+
+get_car = age >= 20 or job
+print(get_car)  # True
+
+get_license = not job
+print(get_license)  # True
+
+score = 50
+# score = score + 10
+
+# Write:
+score += 10
+print(score)  # 70
+
+result = 10 / 2
+print(result)  # 2
+
+ages = 20
+if ages == 18:
+    print("You are an adult.")
+else:
+    print("You are not an adult.")
+
+# String Manipulation
+first_name = "Alice"
+last_name = "Smith"
+agee = 15
+full_name = first_name + " " + last_name
+print(full_name)  # Alice Smith
+
+greet = f"Hello, {first_name}! and i'm {agee} years old."
+print(greet)  # Hello, Alice! and i'm 15 years old.
+
+text = "Python is great!"
+demo = " Python "
+price = "$10.99"
+print(text.upper())  # PYTHON IS GREAT!
+print(text.lower())  # python is great!
+print(demo.strip())  # Python
+print(text.replace("great", "awesome"))  # Python is awesome!
+print(price.startswith("$"))  # True
+print(price.endswith("9"))  # True
+print (text.find("is"))  # 7
+
+print(text.replace("Python", "JavaScript"))
+
 
 first_name = "John"
 last_name = "Doe"
